35bb14d70dc221318dde9ea700558b79216e8ae9
Kristian <kristianjohan@outlook.dk>
2020-02-19 15:20:31 +0100

    Første test...

--- DoorControl.Test.Unit/DoorControlTestClass.cs
+++ DoorControl.Test.Unit/DoorControlTestClass.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DoorControlProject;
+using NUnit.Framework;
+using NSubstitute;
 
-namespace DoorControl.Test.Unit
+namespace DoorControlTest.Unit
 {
+    [TestFixture]
     public class DoorControlTestClass
     {
+        private IDoor _FakeDoor;
+        private IUserValidation _FakeUserValidation;
+        private DoorControl _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _FakeDoor = Substitute.For<IDoor>();
+            _FakeUserValidation = Substitute.For<IUserValidation>();
+
+            _uut =  new DoorControl();
+        }
+
+        [Test]
+        public void EntryIdTest()
+        {
+            _uut.RequestEntry(1234);
+
+            _FakeUserValidation.ValidateEntryRequest(1234).Returns(true);
+
+            _FakeDoor.Received(1).Open();
+        }
+
+
 
     }
 }
